24a4ff8f801b656fee1ee2fb703708a6d541be1e
himanshu-AW <himanshu.pro1810@gmail.com>
2025-05-13 12:19:29 +0530

    fix footer card position

--- src/pages/footerPage/Footer.jsx
+++ src/pages/footerPage/Footer.jsx
@@ -5,20 +5,22 @@ import { ImageFooterBgElements, ImageFeedback } from "../../assets/images";
 function Footer() {
   return (
     <section className="w-full flex items-center relative bg-[#03111A] min-w-[1280px] h-[281px] shadow-sm hover:shadow-lg">
-      <footer className="z-1 w-[999px] h-full absolute top-0 left-[212px] overflow-hidden">
+      <footer className="z-1 w-[999px] h-full absolute top-0 left-[50%] translate-x-[-50%] overflow-hidden">
         <img
           src={ImageFooterBgElements}
           alt={"footer circle designe"}
           className="w-full h-full"
         />
       </footer>
-      <div className="relative z-2 ml-[212px] w-[calc(100%-212px)] pl-[148px] h-full flex pt-[28px] ">
-        <LargeCard2
+      <div className="z-2 ml-[236px] mr-[360px] mx-auto w-full h-full flex justtify-center pt-[28px] ">
+        <div className="w-full flex justify-center">
+          <LargeCard2
           isFooter={true}
           Image={ImageFeedback}
           title={"Give feedback"}
           subTitle={"Suggest featurees, report a problem..."}
         />
+        </div>
       </div>
     </section>
   );
